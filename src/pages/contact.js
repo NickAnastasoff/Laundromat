4e7bef5ffa149670a43a04e03b863077ec2f7018
Nick <anastasoffnick@gmail.com>
2023-03-06 20:40:58 -0500

    working multi page

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import './contact.css';
+
+function Contact() {
+  return (
+    <div>
+      <header>
+        <p>
+          Contact
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Contact;
