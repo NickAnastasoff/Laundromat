38f006e1e6d9ff97397dc7d2b5f4dc003792adc7
Nick <anastasoffnick@gmail.com>
2023-03-06 20:58:10 -0500

    perfectly working multi page template

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Routes, Route}
 	from 'react-router-dom';
 import Home from './pages';
 import Contact from './pages/contact';
+import About from './pages/about';
+import Navbar from './components/navbar/navbar';
 
 function App() {
 return (
 	<Router>
+  <Navbar />
 	<Routes>
 		<Route exact path='/' element={<Home />} />
 		<Route path='/contact' element={<Contact/>} />
+    <Route path='/about' element={<About/>} />
 	</Routes>
 	</Router>
 );
